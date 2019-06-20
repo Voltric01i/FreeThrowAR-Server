19458df3abdf4b545946c6530b69327dace09bbb
Thrioaki <zeus1447@gmail.com>
2019-06-20 19:07:47 +0900

    initial commit

---
+++ FreeThrowAR Server/Assets/Scripts/Network.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+
+public class Network : MonoBehaviour    
+{
+    private TcpListener listener;
+    private List<TcpClient> clients = new List<TcpClient>();
+
+    
+
+
+}
